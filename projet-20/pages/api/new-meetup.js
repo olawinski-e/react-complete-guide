9ab47359793901f905f53936473818126320906d
Olawinski Estelle <estelle.olawinski@me.com>
2021-05-30 13:21:36 +0200

    section 23

--- projet-20/pages/api/new-meetup.js
+++ projet-20/pages/api/new-meetup.js
@@ -1,8 +1,24 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+import PASSWORD from "./../config";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://ewnski:vFpPr7YjE5UDV3da@cluster0.u6ian.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
